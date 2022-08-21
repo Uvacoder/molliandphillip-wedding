9724b82728110be218784ac34aea02417d0cab2b
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-08-21 00:57:30 -0400

    Update where.tsx

--- pages/where.tsx
+++ pages/where.tsx
@@ -62,7 +62,6 @@ export default function Where() {
           width="600" 
           height="450" 
           style={{ border: 0 }}
-          allowFullScreen="" 
           loading="lazy" 
           referrerpolicy="no-referrer-when-downgrade"         
           onLoad={onOpen}
