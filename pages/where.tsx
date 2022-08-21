607dd1b19fda46852ad5127f7c2b5532a50c57a4
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-08-21 00:53:11 -0400

    Update where.tsx

--- pages/where.tsx
+++ pages/where.tsx
@@ -61,7 +61,7 @@ export default function Where() {
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3138.038661189406!2d-78.55711099999999!3d38.13929400000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b47928da4baecd%3A0x25eb155b154bc651!2sCamp%20Albemarle!5e0!3m2!1sen!2sus!4v1661057248767!5m2!1sen!2sus"
           width="600" 
           height="450" 
-          style="border:0;" 
+          style={{ border: 0 }}
           allowfullscreen="" 
           loading="lazy" 
           referrerpolicy="no-referrer-when-downgrade"         
