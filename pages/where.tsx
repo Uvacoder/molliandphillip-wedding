9461dfc20a9c9e508f9b8b33221e36fc26f51446
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-08-21 00:48:10 -0400

    Update where.tsx

--- pages/where.tsx
+++ pages/where.tsx
@@ -58,7 +58,7 @@ export default function Where() {
           </Grid>
         )}
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31733.366770951172!2d106.77524758179645!3d-6.174802399999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f65f648bdc49%3A0x3dfec5a3d3208966!2sPullman%20Jakarta%20Central%20Park!5e0!3m2!1sen!2sid!4v1650877536467!5m2!1sen!2sid"
+          src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3138.038661189406!2d-78.55711099999999!3d38.13929400000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b47928da4baecd%3A0x25eb155b154bc651!2sCamp%20Albemarle!5e0!3m2!1sen!2sus!4v1661057248767!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>"
           width="100%"
           height={isOpen ? 450 : 0}
           style={{ border: 0 }}
